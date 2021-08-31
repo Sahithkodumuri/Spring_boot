781b71eabe43a28e40c8e4ef93ad5b8380cd1a3c
Sahithkodumuri <67045730+Sahithkodumuri@users.noreply.github.com>
2021-08-31 17:17:22 +0530

    Add files via upload

---
+++ main/java/com/cts/spring/model/Employee.java
@@ -0,0 +1,68 @@
+package com.cts.spring.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="employees")
+public class Employee {
+
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name="first_name")
+	private String firstname;
+	
+	@Column(name="last_name")
+	private String lastname;
+	
+	@Column(name="emailId")
+	private String emailId;
+	
+	public Employee() {
+		
+	}
+	
+	public Employee(int id, String firstname, String lastname, String emailId) {
+		super();
+		this.id = id;
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.emailId = emailId;
+	}
+
+
+
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	
+	
+}
